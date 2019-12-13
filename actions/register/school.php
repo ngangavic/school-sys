fde48f07e5ab0baa1b1bf82c12f8a6801994e693
ngangavic <ngangavictor10@gmail.com>
2019-12-13 14:46:43 +0300

    school registration data validation

--- actions/register/school.php
+++ actions/register/school.php
@@ -4,39 +4,57 @@ require_once "../database/connection.php";
 //1. check if variables are empty
 //2. check if variables are set
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$box = $_POST['address'];
-$town = $_POST['town'];
-$population = $_POST['population'];
-$password = $_POST['pass1'];
-$register = $_POST['register'];
+$name = cleanData($_POST['name']);
+$email = cleanData($_POST['email']);
+$phone = cleanData($_POST['phone']);
+$box = cleanData($_POST['address']);
+$town = cleanData($_POST['town']);
+$population = cleanData($_POST['population']);
+$password = cleanData($_POST['pass1']);
+$register = cleanData($_POST['register']);
 
-if (isset($name) && isset($email) && isset($phone) && isset($box) && isset($town) && isset($population) && isset($password) && isset($register)) {
-//Check if school already exists.
+if (!isset($name) || !isset($email) || !isset($phone) || !isset($box) || !isset($town) || !isset($population) || !isset($password) || !isset($register)) {
+    echo 'empty values';
+} else {
+    $count = checkIfExists($conn, $name, $phone, $email);
+    if ($count == 0) {
+        $response = registerSchool($conn, $name, $phone, $box, $town, $email, $population);
+        if ($response == 0) {
+            echo 'success';
+        } else {
+            echo 'not inserted';
+        }
+    } else {
+        echo 'already exists';
+    }
+    //header("location: ../../register/index.html");
+}
+
+
+function checkIfExists($conn, $name, $phone, $email)
+{
     $stmt = $conn->prepare("SELECT * FROM tbl_school WHERE name=? OR phone=? OR email=?");
     $stmt->bind_param("sss", $name, $phone, $email);
     $stmt->execute();
     $result = $stmt->get_result();
     $count = $result->num_rows;
-    if ($count == 0) {
-//register
-        $stmt = $conn->prepare("INSERT INTO tbl_school(name,phone,box,town,email,population,date) VALUES (?,?,?,?,?,?,CURRENT_TIMESTAMP )");
-        $stmt->bind_param("ssssss", $name, $phone, $box, $town, $email, $population);
-        if (!$stmt->execute()) {
-            echo 'failed';
-        } else {
-            echo 'registered';
-        }
-    } else {
-        //already exists
-        echo 'already exists';
-    }
-
-
-} else {
-    echo 'empty values';
-    header("location: ../../register/index.html");
+    return $count;
 }
 
+function registerSchool($conn, $name, $phone, $box, $town, $email, $population)
+{
+    $stmt = $conn->prepare("INSERT INTO tbl_school(name,phone,box,town,email,population,date) VALUES (?,?,?,?,?,?,CURRENT_TIMESTAMP )");
+    $stmt->bind_param("ssssss", $name, $phone, $box, $town, $email, $population);
+    if (!$stmt->execute()) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function cleanData($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
