b74a24399e7ec731530d378cb26f533f542bc307
ngangavic <ngangavictor10@gmail.com>
2020-02-01 17:52:08 +0300

    admin cards

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
         <link href="style.css" rel="stylesheet">
         <link href="../fonts/css/all.css" rel="stylesheet">
         <style>
-            a{
+            a {
                 text-decoration: none;
             }
         </style>
@@ -53,10 +53,11 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
     </nav>
     <!--[END] navbar-->
 
-<!--    new UI start-->
+    <!--    new UI start-->
     <div class="container-fluid">
 
         <div class="row">
+
             <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 sidebar">
                 <div class="link-item">
                     <a href=""><i class="fa fa-envelope"></i> SMS </a>
@@ -92,15 +93,57 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
                 </div>
 
 
-
-
             </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 main-content">
+                <div class="row">
+
+                    <div class="card col-md-2 my-card">
+                        <div class="card-body">
+                            <div class="row">
+                                <h2>0</h2>
+                                <i class="fa fa-3x fa-users ml-auto"></i>
+                            </div>
+                            Classes
+                        </div>
+                    </div>
+                    <div class="card col-md-2 my-card">
+                        <div class="card-body">
+                            <div class="row">
+                                <h2>0</h2>
+                                <i class="fa fa-3x fa-user ml-auto"></i>
+                            </div>
+                            Students
+                        </div>
+                    </div>
+                    <div class="card col-md-2 my-card">
+                        <div class="card-body">
+                            <div class="row">
+                                <h2>0</h2>
+                                <i class="fa fa-3x fa-male ml-auto"></i>
+                            </div>
+                            Teachers
+                        </div>
+                    </div>
+                    <div class="card col-md-2 my-card">
+                        <div class="card-body">
+                            <div class="row">
+                                <h2>0</h2>
+                                <i class="fa fa-3x fa-home ml-auto"></i>
+                            </div>
+                            Parents
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+
         </div>
 
     </div>
-<!--    new UI end-->
-
-
+    <!--    new UI end-->
 
 
     <!--[START] content-->
@@ -123,42 +166,42 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
             <!--        [START] card-->
             <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
                 <a href="class.php" style="text-decoration: none;color: #000000">
-                <div class="card">
-                    <div class="card-header">
-                        Classes
+                    <div class="card">
+                        <div class="card-header">
+                            Classes
+                        </div>
+                        <div class="card-body red">
+                            Add class,Edit class,Class teachers...
+                        </div>
                     </div>
-                    <div class="card-body red">
-                        Add class,Edit class,Class teachers...
-                    </div>
-                </div>
                 </a>
             </div>
             <!--        [END] card-->
             <!--        [START] card-->
             <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
                 <a href="subject.php" style="text-decoration: none;color: #000000">
-                <div class="card">
-                    <div class="card-header">
-                        Subjects
+                    <div class="card">
+                        <div class="card-header">
+                            Subjects
+                        </div>
+                        <div class="card-body red">
+                            Grades,Teachers,Subject selection...
+                        </div>
                     </div>
-                    <div class="card-body red">
-                        Grades,Teachers,Subject selection...
-                    </div>
-                </div>
                 </a>
             </div>
             <!--        [END] card-->
             <!--        [START] card-->
             <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
                 <a href="student.php?page=add-student" style="text-decoration: none;color: #000000">
-                <div class="card">
-                    <div class="card-header">
-                        Students
+                    <div class="card">
+                        <div class="card-header">
+                            Students
+                        </div>
+                        <div class="card-body red">
+                            Add, Edit,View,Search student...
+                        </div>
                     </div>
-                    <div class="card-body red">
-                        Add, Edit,View,Search student...
-                    </div>
-                </div>
                 </a>
             </div>
             <!--        [END] card-->
@@ -180,15 +223,15 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
             <!--        [END] card-->
             <!--        [START] card-->
             <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-                <a data-toggle="modal" href="#resultsModal" style="text-decoration: none;color: #000000" >
-                <div class="card">
-                    <div class="card-header">
-                        Results
+                <a data-toggle="modal" href="#resultsModal" style="text-decoration: none;color: #000000">
+                    <div class="card">
+                        <div class="card-header">
+                            Results
+                        </div>
+                        <div class="card-body red">
+                            Enter results, Edit results...
+                        </div>
                     </div>
-                    <div class="card-body red">
-                        Enter results, Edit results...
-                    </div>
-                </div>
                 </a>
             </div>
             <!--        [END] card-->
@@ -207,14 +250,14 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
             <!--        [START] card-->
             <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
                 <a href="exams.php" style="text-decoration: none;color: #000000">
-                <div class="card">
-                    <div class="card-header">
-                        Exams
+                    <div class="card">
+                        <div class="card-header">
+                            Exams
+                        </div>
+                        <div class="card-body red">
+                            Create exam...
+                        </div>
                     </div>
-                    <div class="card-body red">
-                        Create exam...
-                    </div>
-                </div>
                 </a>
             </div>
             <!--        [END] card-->
@@ -224,7 +267,7 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
     </div>
     <!--[END] content-->
 
-<!--    [START]results modal-->
+    <!--    [START]results modal-->
 
     <!-- Modal -->
     <div id="resultsModal" class="modal fade" role="dialog">
@@ -241,54 +284,54 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
 
                         <select name="exam" class="form-control">
                             <?php
-                            $stmt=$conn->prepare("SELECT * FROM tbl_exam WHERE school=?");
-                            $stmt->bind_param("s",$_SESSION['id']);
+                            $stmt = $conn->prepare("SELECT * FROM tbl_exam WHERE school=?");
+                            $stmt->bind_param("s", $_SESSION['id']);
                             $stmt->execute();
-                            $result=$stmt->get_result();
-                            while ($row=$result->fetch_array()){
-                            ?>
-                            <option value="<?php echo $row['name']; ?>"><?php echo $row['name'] ?></option>
-                                <?php } ?>
+                            $result = $stmt->get_result();
+                            while ($row = $result->fetch_array()) {
+                                ?>
+                                <option value="<?php echo $row['name']; ?>"><?php echo $row['name'] ?></option>
+                            <?php } ?>
                         </select>
 
                         <select name="class" class="form-control">
                             <?php
-                            $stmt=$conn->prepare("SELECT * FROM tbl_class WHERE school=?");
-                            $stmt->bind_param("s",$_SESSION['id']);
+                            $stmt = $conn->prepare("SELECT * FROM tbl_class WHERE school=?");
+                            $stmt->bind_param("s", $_SESSION['id']);
                             $stmt->execute();
-                            $result=$stmt->get_result();
-                            while ($row=$result->fetch_array()){
+                            $result = $stmt->get_result();
+                            while ($row = $result->fetch_array()) {
                                 ?>
                                 <option value="<?php echo $row['name']; ?>"><?php echo $row['name'] ?></option>
                             <?php } ?>
                         </select>
 
                         <select name="term" class="form-control">
-                                <option value="Term 1">Term 1</option>
-                                <option value="Term 2">Term 2</option>
-                                <option value="Term 3">Term 3</option>
+                            <option value="Term 1">Term 1</option>
+                            <option value="Term 2">Term 2</option>
+                            <option value="Term 3">Term 3</option>
                         </select>
 
                         <select name="year" class="form-control">
-                                <option value="2017">2017</option>
-                                <option value="2018">2018</option>
-                                <option value="2019">2019</option>
-                                <option value="2020">2020</option>
+                            <option value="2017">2017</option>
+                            <option value="2018">2018</option>
+                            <option value="2019">2019</option>
+                            <option value="2020">2020</option>
                         </select>
 
                         <select name="subject" class="form-control">
                             <?php
-                            $stmt=$conn->prepare("SELECT * FROM tbl_subject WHERE school=?");
-                            $stmt->bind_param("s",$_SESSION['id']);
+                            $stmt = $conn->prepare("SELECT * FROM tbl_subject WHERE school=?");
+                            $stmt->bind_param("s", $_SESSION['id']);
                             $stmt->execute();
-                            $result=$stmt->get_result();
-                            while ($row=$result->fetch_array()){
+                            $result = $stmt->get_result();
+                            while ($row = $result->fetch_array()) {
                                 ?>
                                 <option value="<?php echo $row['name']; ?>"><?php echo $row['name'] ?></option>
                             <?php } ?>
                         </select>
 
-                        <button type="submit" class="btn btn-outline-primary" >DONE</button>
+                        <button type="submit" class="btn btn-outline-primary">DONE</button>
                     </form>
                 </div>
                 <div class="modal-footer">
@@ -298,7 +341,7 @@ if (isset($_SESSION['email']) && isset($_SESSION['id']) && isset($_SESSION['name
 
         </div>
     </div>
-<!--[END]results modal-->
+    <!--[END]results modal-->
 
 
     </body>
